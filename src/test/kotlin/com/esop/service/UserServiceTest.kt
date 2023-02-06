b77c978752b06da35cb8c443d057091f7ced615f
sankar <sankaranarayananm@sahaj.ai>
2023-02-06 18:38:37 +0530

    Add: Test for add money to wallet

--- src/test/kotlin/com/esop/service/UserServiceTest.kt
+++ src/test/kotlin/com/esop/service/UserServiceTest.kt
@@ -1,21 +1,22 @@
 package com.esop.service
 
+import com.esop.dto.AddWalletDTO
 import com.esop.dto.UserCreationDTO
 import com.esop.schema.Order
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import jakarta.inject.Inject
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 @MicronautTest
-class UserServiceTest{
+class UserServiceTest {
 
     @Inject
     private lateinit var userService: UserService
 
     @Test
-    fun `should register a valid user`(){
-        val user = UserCreationDTO("Sankar","M","+917550276216","sankar@sahaj.ai","sankar06")
+    fun `should register a valid user`() {
+        val user = UserCreationDTO("Sankar", "M", "+917550276216", "sankar@sahaj.ai", "sankar06")
         val expected = mapOf(
             "firstName" to "Sankar",
             "lastName" to "M",
@@ -28,11 +29,11 @@ class UserServiceTest{
         val response = userService.registerUser(user)
 
         //Assert
-        assertEquals(response,expected)
+        assertEquals(response, expected)
     }
 
     @Test
-    fun `should check user doesn't exist before placing Order`(){
+    fun `should check user doesn't exist before placing Order`() {
         val order = Order(
             quantity = 10,
             type = "BUY",
@@ -44,5 +45,19 @@ class UserServiceTest{
         val errors = UserService.orderCheckBeforePlace(order)
 
         assertEquals(expectedErrors, errors, "user non existent error should be present in the errors list")
+    }
+    
+    @Test
+    fun `should add money to wallet`() {
+        val user = UserCreationDTO("Sankar", "M", "+917550276216", "sankar@sahaj.ai", "Sankar")
+        userService.registerUser(user)
+        val walletDetails = AddWalletDTO(price = 1000)
+        val expectedFreeMoney: Long = 1000
+        val expectedUsername = "Sankar"
+
+        userService.addingMoney(walletDetails, "Sankar")
+
+        val actualFreeMoney = UserService.userList[expectedUsername]!!.userWallet.getFreeMoney()
+        assertEquals(expectedFreeMoney, actualFreeMoney)
     }
 }
