c09a6cb1a7866cc604b08ba8ce19c48c512cd48b
kajalp238 <kajalp@sahaj.ai>
2023-02-07 15:34:02 +0530

    Add: move ESOPs from locked state of one user to another

---
+++ src/test/kotlin/com/esop/schema/UserTest.kt
@@ -0,0 +1,20 @@
+package com.esop.schema
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class UserTest {
+
+    @Test
+    fun `it should transfer Performance ESOPs to the other user`() {
+        val userOne = User("Sankaranarayanan", "M", "7550276216", "sankaranarayananm@sahaj.ai", "sankar")
+        val userTwo = User("Aditya", "Tiwari", "", "aditya@sahaj.ai", "aditya")
+        userOne.userPerformanceInventory.addESOPsToInventory(20)
+        userOne.userPerformanceInventory.moveESOPsFromFreeToLockedState(10)
+
+        userOne.transferLockedESOPsTo(userTwo, "PERFORMANCE", 10)
+
+        Assertions.assertEquals(10, userOne.userPerformanceInventory.getFreeInventory())
+        Assertions.assertEquals(10, userTwo.userNonPerfInventory.getFreeInventory())
+    }
+}
