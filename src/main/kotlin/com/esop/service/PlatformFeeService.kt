ded3c26c01e5df6a291859db05faefc9babe0761
kajalp238 <kajalp@sahaj.ai>
2023-02-08 17:06:14 +0530

    Add: PlatformFeeService

---
+++ src/main/kotlin/com/esop/service/PlatformFeeService.kt
@@ -0,0 +1,29 @@
+package com.esop.service
+
+
+import com.esop.constant.FEE_PERCENTAGE
+import jakarta.inject.Singleton
+import java.math.BigInteger
+import kotlin.math.round
+
+
+@Singleton
+class PlatformFeeService {
+
+    private var totalPlatformFee: BigInteger = BigInteger("0")
+
+    fun getPlatformFee(): BigInteger {
+        return totalPlatformFee
+    }
+
+    private fun calculateFee(tradedAmount: Long) = round(tradedAmount * FEE_PERCENTAGE).toLong()
+
+    fun deductPlatformFeeFrom(tradedAmount: Long, esopType: String): Long {
+        if (tradedAmount < 0) throw IllegalArgumentException("Traded Amount cannot be negative")
+        if (esopType == "PERFORMANCE") return tradedAmount
+
+        val fee = calculateFee(tradedAmount)
+        totalPlatformFee += fee.toBigInteger()
+        return tradedAmount - fee
+    }
+}
---
+++ src/test/kotlin/com/esop/service/PlatformFeeServiceTest.kt
@@ -0,0 +1,51 @@
+package com.esop.service
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.math.BigInteger
+
+
+class PlatformFeeServiceTest {
+
+    private lateinit var platformFeeService: PlatformFeeService
+
+    @BeforeEach
+    fun `it should set the Platform fee to zero`() {
+        platformFeeService = PlatformFeeService()
+    }
+
+    @Test
+    fun `it should deduct platform fee from the given amount as ESOP type is NON_PERFORMANCE`() {
+        val amount = 1000L
+
+        val actualAmount = platformFeeService.deductPlatformFeeFrom(amount, "NON_PERFORMANCE")
+
+        val expectedAmount = 980L
+        val expectedPlatformFee = BigInteger("20")
+        assertEquals(expectedAmount, actualAmount)
+        assertEquals(expectedPlatformFee, platformFeeService.getPlatformFee())
+    }
+
+    @Test
+    fun `it should not deduct platform fee from the given amount as ESOP type is PERFORMANCE`() {
+        val amount = 1000L
+
+        val actualAmount = platformFeeService.deductPlatformFeeFrom(amount, "PERFORMANCE")
+
+        val expectedAmount = 1000L
+        val expectedPlatformFee = BigInteger("0")
+        assertEquals(expectedAmount, actualAmount)
+        assertEquals(expectedPlatformFee, platformFeeService.getPlatformFee())
+    }
+
+    @Test
+    fun `it should throw exception if a invalid traded amount is being passed`() {
+        val tradedAmount: Long = -100
+
+        Assertions.assertThrows(IllegalArgumentException::class.java) {
+            platformFeeService.deductPlatformFeeFrom(tradedAmount, "NON_PERFORMANCE")
+        }
+    }
+}
