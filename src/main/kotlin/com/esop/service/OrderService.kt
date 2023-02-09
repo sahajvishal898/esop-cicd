a2ff8c1cebfa4f34957d4a5ff8f080b6d0b1726f
kajalp238 <kajalp@sahaj.ai>
2023-02-09 14:22:37 +0530

    Refactor: Order Service

--- src/main/kotlin/com/esop/service/OrderService.kt
+++ src/main/kotlin/com/esop/service/OrderService.kt
@@ -4,26 +4,23 @@ package com.esop.service
 import com.esop.constant.errors
 import com.esop.repository.OrderRecords
 import com.esop.repository.UserRecords
-import com.esop.schema.*
+import com.esop.schema.ESOPType
+import com.esop.schema.History
+import com.esop.schema.Order
+import com.esop.schema.OrderStatus.COMPLETED
+import com.esop.schema.OrderType.BUY
 import com.esop.schema.User
 import jakarta.inject.Singleton
 import kotlin.math.min
-import com.esop.schema.ESOPType.*
-import com.esop.schema.OrderStatus.*
-import com.esop.schema.OrderType.*
 
 @Singleton
-class OrderService(private val userRecords: UserRecords,
-                   private val orderRecords: OrderRecords,
-                   private val platformFeeService: PlatformFeeService
+class OrderService(
+    private val userRecords: UserRecords,
+    private val orderRecords: OrderRecords,
+    private val platformFeeService: PlatformFeeService
 ) {
 
-    private fun updateOrderDetails(
-        currentTradeQuantity: Long,
-        sellerOrder: Order,
-        buyerOrder: Order
-    ) {
-        // Deduct money of quantity taken from buyer
+    private fun updateUserDetails(currentTradeQuantity: Long, sellerOrder: Order, buyerOrder: Order) {
         val sellAmount = sellerOrder.getPrice() * (currentTradeQuantity)
         val buyer = userRecords.getUser(buyerOrder.getUserName())!!
         val seller = userRecords.getUser(sellerOrder.getUserName())!!
@@ -34,14 +31,10 @@ class OrderService(private val userRecords: UserRecords,
 
         val amountToBeReleased = (buyerOrder.getPrice() - sellerOrder.getPrice()) * (currentTradeQuantity)
         buyer.userWallet.moveMoneyFromLockedToFree(amountToBeReleased)
-
     }
 
     private fun updateWalletBalances(
-        sellAmount: Long,
-        esopType: ESOPType,
-        buyer: User,
-        seller: User
+        sellAmount: Long, esopType: ESOPType, buyer: User, seller: User
     ) {
         val adjustedSellAmount = platformFeeService.deductPlatformFeeFrom(sellAmount, esopType)
 
@@ -64,24 +57,17 @@ class OrderService(private val userRecords: UserRecords,
 
     private fun executeBuyOrder(buyOrder: Order) {
         orderRecords.addBuyOrder(buyOrder)
-        while(buyOrder.remainingQuantity != 0L){
-            val bestSellOrder = orderRecords.getBestSellOrder(buyOrder.getPrice())?:return
-            performOrderMatching(
-                bestSellOrder,
-                buyOrder
-            )
+        while (buyOrder.remainingQuantity != 0L) {
+            val bestSellOrder = orderRecords.getBestSellOrder(buyOrder.getPrice()) ?: return
+            performOrderMatching(bestSellOrder, buyOrder)
         }
     }
 
     private fun executeSellOrder(sellOrder: Order) {
         orderRecords.addSellOrder(sellOrder)
-        while(sellOrder.remainingQuantity != 0L){
-            val bestBuyOrder = orderRecords.getBestBuyOrder(sellOrder.getPrice())?:return
-
-            performOrderMatching(
-                sellOrder,
-                bestBuyOrder
-            )
+        while (sellOrder.remainingQuantity != 0L) {
+            val bestBuyOrder = orderRecords.getBestBuyOrder(sellOrder.getPrice()) ?: return
+            performOrderMatching(sellOrder, bestBuyOrder)
         }
     }
 
@@ -94,12 +80,12 @@ class OrderService(private val userRecords: UserRecords,
 
         createOrderFilledLogs(orderExecutionQuantity, orderExecutionPrice, sellOrder, buyOrder)
 
-        updateOrderDetails(
-            orderExecutionQuantity,
-            sellOrder,
-            buyOrder
-        )
+        updateUserDetails(orderExecutionQuantity, sellOrder, buyOrder)
 
+        removeCompletedOrders(buyOrder, sellOrder)
+    }
+
+    private fun removeCompletedOrders(buyOrder: Order, sellOrder: Order) {
         if (buyOrder.orderStatus == COMPLETED) {
             orderRecords.removeBuyOrder(buyOrder)
         }
@@ -109,10 +95,7 @@ class OrderService(private val userRecords: UserRecords,
     }
 
     private fun createOrderFilledLogs(
-        orderExecutionQuantity: Long,
-        orderExecutionPrice: Long,
-        sellOrder: Order,
-        buyOrder: Order
+        orderExecutionQuantity: Long, orderExecutionPrice: Long, sellOrder: Order, buyOrder: Order
     ) {
         buyOrder.updateOrderLogs(orderExecutionQuantity, orderExecutionPrice, sellOrder)
         sellOrder.updateOrderLogs(orderExecutionQuantity, orderExecutionPrice, buyOrder)
