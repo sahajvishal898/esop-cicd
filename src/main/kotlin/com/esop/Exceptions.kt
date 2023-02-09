1660ca927c375cf749fe555bd287da490d9768e2
VighneshPath <pathrikarvighnesh@gmail.com>
2023-02-09 11:24:49 +0530

    Add: Invalid Wallet and Inventory Exceptions

--- src/main/kotlin/com/esop/Exceptions.kt
+++ src/main/kotlin/com/esop/Exceptions.kt
@@ -1,5 +1,6 @@
 package com.esop
 
+import com.esop.schema.ESOPType
 import io.micronaut.http.HttpStatus
 
 open class HttpException(val status: HttpStatus, message: String) : RuntimeException(message)
@@ -9,6 +10,10 @@ class InventoryLimitExceededException : HttpException(HttpStatus.BAD_REQUEST, "I
 class WalletLimitExceededException : HttpException(HttpStatus.BAD_REQUEST, "Wallet Limit exceeded")
 
 class PlatformFeeLessThanZeroException : Exception("Platform fee cannot be less than zero")
+
+class InsufficientFundsException(val errorMessage: String = "Insufficient Funds"): Throwable()
+
+class InsufficientInventoryException(val type: ESOPType): Throwable("Insufficient $type inventory"){}
 
 class UserDoesNotExistException(val errorMessage: String): Throwable()
 
